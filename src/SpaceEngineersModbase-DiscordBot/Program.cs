c17fdd09b8126d0f303db8e0a24fa420d78dd648
Marco-Zechner <marcoflorian00956@gmail.com>
2024-12-24 10:01:50 +0100

    Created Program.cs

--- src/SpaceEngineersModbase-DiscordBot/Program.cs
+++ src/SpaceEngineersModbase-DiscordBot/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace MarcoZechner.DiscordBot {
+
+    class Program{
+        static async Task Main(string[] args) {
+            // var client = new DiscordSocketClient();
+            // client.Log += LogAsync;
+            // client.Ready += ReadyAsync;
+            // client.MessageReceived += MessageReceivedAsync;
+
+            // var token = "YOUR_TOKEN_HERE";
+            // await client.LoginAsync(TokenType.Bot, token);
+            // await client.StartAsync();
+
+            // await Task.Delay(-1);
+
+            return;
+        }
+    }
+}
